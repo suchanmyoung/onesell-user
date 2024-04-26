49aeb3ee9bdae1d83e9b8ede4a47ceb81cb8ab89
suchanmyoung <aud248@naver.com>
2024-04-27 01:55:36 +0900

    test: 회원 수정 API 인수테스트

--- src/main/java/com/onesell/user/user/dto/UserModifyRequest.java
+++ src/main/java/com/onesell/user/user/dto/UserModifyRequest.java
@@ -1,8 +1,14 @@
 package com.onesell.user.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserModifyRequest {
 
     private String password;
--- src/test/java/com/onesell/user/user/step/UserStep.java
+++ src/test/java/com/onesell/user/user/step/UserStep.java
@@ -1,19 +1,42 @@
 package com.onesell.user.user.step;
 
+import static com.onesell.user.common.Step.*;
+
+import com.onesell.user.common.AcceptanceTest;
+import com.onesell.user.common.Step;
 import com.onesell.user.user.dto.UserJoinRequest;
+import com.onesell.user.user.dto.UserModifyRequest;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
-public class UserStep {
+public class UserStep extends Step {
 
-    public static ExtractableResponse<Response> 회원가입_요청(final UserJoinRequest userJoinRequest){
+    public static ExtractableResponse<Response> 회원가입_요청(final UserJoinRequest userJoinRequest) {
         return RestAssured
             .given().log().all()
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .body(userJoinRequest)
             .when().post("/api/user/join")
+            .then().log().all()
+            .extract();
+    }
+
+    public static String 사용자가_회원가입함(final UserJoinRequest userJoinRequest) {
+        final ExtractableResponse<Response> response = 회원가입_요청(userJoinRequest);
+        응답_코드_검증(response, HttpStatus.CREATED);
+        return response.jsonPath().getString("data.userId");
+    }
+
+    public static ExtractableResponse<Response> 회원_수정_요청(final String userId,
+        final UserModifyRequest userModifyRequest) {
+        return RestAssured
+            .given().log().all()
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .body(userModifyRequest)
+            .when().patch("/api/user/" + userId)
             .then().log().all()
             .extract();
     }
