30b23f51344985f93f1a484fa1b1543af9fa863a
suchanmyoung <aud248@naver.com>
2024-04-27 03:56:32 +0900

    feat: Audit 설정

---
+++ src/main/java/com/onesell/user/common/audit/AuditConfig.java
@@ -0,0 +1,10 @@
+package com.onesell.user.common.audit;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@Component
+@EnableJpaAuditing
+public class AuditConfig {
+
+}
---
+++ src/main/java/com/onesell/user/common/audit/AuditEntity.java
@@ -0,0 +1,25 @@
+package com.onesell.user.common.audit;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditEntity {
+
+    @CreatedDate
+    @Column(name = "CREATED_AT", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "MODIFIED_AT", nullable = false)
+    private LocalDateTime modifiedAt;
+
+}
