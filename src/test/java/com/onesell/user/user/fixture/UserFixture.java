ec7bd0364b73bb37c50f750df054dcdddeeec4e3
suchanmyoung <aud248@naver.com>
2024-04-28 04:12:11 +0900

    fix: 유효성 검사 추가 후 테스트 픽스처 수정

--- src/test/java/com/onesell/user/user/fixture/UserFixture.java
+++ src/test/java/com/onesell/user/user/fixture/UserFixture.java
@@ -9,62 +9,62 @@ public class UserFixture {
 
     public static UserJoinRequest 회원가입_요청_픽스처(){
         return UserJoinRequest.builder()
-            .userId("ID")
-            .password("PASSWORD")
+            .userId("ID001")
+            .password("PASSWORD001")
             .nickname("NICKNAME")
-            .email("EMAIL")
+            .email("EMAIL@google.com")
             .name("NAME")
-            .cellphone("CELLPHONE")
+            .cellphone("010-0000-0000")
             .build();
     }
 
     public static List<UserJoinRequest> 회원가입_요청_필수값_누락_픽스처() {
         final UserJoinRequest ID_누락 = UserJoinRequest.builder()
-            .password("PASSWORD")
+            .password("PASSWORD001")
             .nickname("NICKNAME")
-            .email("EMAIL")
+            .email("EMAIL@google.com")
             .name("NAME")
-            .cellphone("CELLPHONE")
+            .cellphone("010-0000-0000")
             .build();
 
         final UserJoinRequest PASSWORD_누락 = UserJoinRequest.builder()
-            .userId("ID")
+            .userId("ID001")
             .nickname("NICKNAME")
-            .email("EMAIL")
+            .email("EMAIL@google.com")
             .name("NAME")
-            .cellphone("CELLPHONE")
+            .cellphone("010-0000-0000")
             .build();
 
         final UserJoinRequest NICKNAME_누락 = UserJoinRequest.builder()
-            .userId("ID")
-            .password("PASSWORD")
-            .email("EMAIL")
+            .userId("ID001")
+            .password("PASSWORD001")
+            .email("EMAIL@google.com")
             .name("NAME")
-            .cellphone("CELLPHONE")
+            .cellphone("010-0000-0000")
             .build();
 
         final UserJoinRequest NAME_누락 = UserJoinRequest.builder()
-            .userId("ID")
-            .password("PASSWORD")
+            .userId("ID001")
+            .password("PASSWORD001")
             .nickname("NICKNAME")
-            .email("EMAIL")
-            .cellphone("CELLPHONE")
+            .email("EMAIL@google.com")
+            .cellphone("010-0000-0000")
             .build();
 
         final UserJoinRequest EMAIL_누락 = UserJoinRequest.builder()
-            .userId("ID")
-            .password("PASSWORD")
+            .userId("ID001")
+            .password("PASSWORD001")
             .nickname("NICKNAME")
             .name("NAME")
-            .cellphone("CELLPHONE")
+            .cellphone("010-0000-0000")
             .build();
 
         final UserJoinRequest CELLPHONE_누락 = UserJoinRequest.builder()
-            .userId("ID")
-            .password("PASSWORD")
+            .userId("ID001")
+            .password("PASSWORD001")
             .nickname("NICKNAME")
+            .email("EMAIL@google.com")
             .name("NAME")
-            .email("EMAIL")
             .build();
 
         return Arrays.asList(ID_누락, PASSWORD_누락, NICKNAME_누락, EMAIL_누락, NAME_누락, CELLPHONE_누락);
@@ -72,12 +72,12 @@ public class UserFixture {
 
     public static UserJoinRequest 회원가입_중복_ID_픽스처() {
         return UserJoinRequest.builder()
-            .userId("ID")
-            .password("ANOTHER_PASSWORD")
-            .nickname("ANOTHER_NICKNAME")
-            .email("ANOTHER_EMAIL")
-            .name("ANOTHER_NAME")
-            .cellphone("ANOTHER_CELLPHONE")
+            .userId("ID001")
+            .password("PASSWORD001")
+            .nickname("NICKNAME")
+            .email("EMAIL@google.com")
+            .name("NAME")
+            .cellphone("010-0000-0000")
             .build();
     }
 
